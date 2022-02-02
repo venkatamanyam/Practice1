974e607e1648667862593f7fea9ef96b7c501095
venkatamanyam <kvsubbu.db2@gmail.com>
2022-02-02 18:46:49 +0530

    stasheschanges

--- Tapes/src/com/AS.java
+++ Tapes/src/com/AS.java
@@ -4,7 +4,16 @@ public class AS {
 
 	public static void main(String[] args) {
         System.out.println("sai");
+
         System.out.println("Ramu");
+
+        System.out.println("Ram");
+
 	}
+	public static void m1() 
+	{
+		System.out.println("stash");
+	}
+	
 
 }
