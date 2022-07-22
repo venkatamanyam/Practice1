4cead4950957e831a121f7b158f697551b735555
venkatamanyam <kvsubbu.db2@gmail.com>
2022-07-22 12:36:21 +0530

    com2

--- jobdetails/src/main/java/com/project/ems/jobdetails/controller/CandidateDetailsController.java
+++ jobdetails/src/main/java/com/project/ems/jobdetails/controller/CandidateDetailsController.java
@@ -24,6 +24,7 @@ public class CandidateDetailsController {
 	@GetMapping("/candidate")
 	public List<CandidateDetailsDomain> listAll()
 	{
+		System.out.println("hi");
 		return service.listAll();
 	}
 	
